1306e25cffe9fd9e4efe0a442d27a84b172d1dbb
DFrOsT <david_was@me.com>
2012-05-05 19:12:24 +0200

    Added Console Showcase

---
+++ ConsoleShowCase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Models.EF;
+
+namespace ConsoleShowCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new xLingua_StagingEntities())
+            {
+                var basewords = from b in context.Basewords1
+                                where b.WordtypeId == 1 && b.LanguageId == 1
+                                select b;
+
+                foreach (var baseword in basewords)
+                {
+                    Console.Out.WriteLine(baseword.Text);
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
