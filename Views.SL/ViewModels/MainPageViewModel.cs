6f1bb5892fead4a46190bdbd576a29cd21aa0681
DFrOsT <david_was@me.com>
2012-05-19 16:56:51 +0200

    changed configuration

--- Views.SL/ViewModels/MainPageViewModel.cs
+++ Views.SL/ViewModels/MainPageViewModel.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
-using Views.SL.ModelService;
+using Views.SL.ModelServiceReference;
 
 namespace Views.SL.ViewModels
 {
